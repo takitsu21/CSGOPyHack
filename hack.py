fd7484c2e1df808e5269efa1ff2c49e192710d01
takitsu21 <dylannbatisse@gmail.com>
2019-07-30 15:28:26 +0200

    add noflash, thread process, auto offsets updater

--- hack.py
+++ hack.py
@@ -1,66 +1,151 @@
+# !/usr/bin/python
+# coding:utf8
 import pymem
 import time
-from src.memory_adresses import *
+import keyboard
 from threading import Thread
 import threading
+import json
+import datetime
+import logging
+import sys
+import os
+import requests
+
+offset_update = "https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json"
+
+def is_updated(offsets, local_offsets):
+    print("Checking for updates...")
+    r = requests.get(offsets)
+    timestamp = json.loads(r.text)
+    return local_offsets["timestamp"] == timestamp["timestamp"]
 
 
-class Wallhack:
-    def __init__(self, pm=None, client=None):
-        self.pm = pm
-        self.client = client
-        self.run_event = threading.Event()
-        self.run_event.set()
-        thread = Thread(target=self._wh, args=())
-        thread.start()
+def _update(offsets):
+    print("Downloading...")
+    r = requests.get(offsets)
+    if r.status_code == 200:
+        with open("src/offsets.json", "w+", encoding="utf8") as f:
+            f.write(r.text)
+        print("offsets downloaded!\n")
+    else:
+        print(f"ERROR {r.status_code} ON DOWNLOAD")
+
+try:
+    with open("src/offsets.json", "r", encoding="utf8") as f:
+        offsets = json.loads(str(''.join(f.readlines())))
+except Exception as e:
+    print(f"{type(e).__name__} : {type(e)}")
+    _update(offset_update)
+    with open("src/offsets.json", "r", encoding="utf8") as f:
+        offsets = json.loads(str(''.join(f.readlines())))
+
+QUIT = False
+netvars = offsets["netvars"]
+signatures = offsets["signatures"]
+
+m_iTeamNum = netvars["m_iTeamNum"]
+m_iGlowIndex = netvars["m_iGlowIndex"]
+dwEntityList = signatures["dwEntityList"]
+dwForceAttack = signatures["dwForceAttack"]
+m_iCrosshairId = netvars["m_iCrosshairId"]
+dwLocalPlayer = signatures["dwLocalPlayer"]
+dwGlowObjectManager = signatures["dwGlowObjectManager"]
+m_flFlashMaxAlpha = netvars["m_flFlashMaxAlpha"]
+
+
+def last_update(timestamp):
+    date = datetime.datetime.fromtimestamp(timestamp)
+    return f"{date.day}/{date.month}/{date.year}"
+
+class AimLock(threading.Thread):
+    def __init__(self):
+        super (AimLock, self).__init__ ()
+
+        print("Aimlock enabled... press ² to use it")
+    
+    def lock(self):
+        pass
+
+class AntiFlash(threading.Thread):
+    def __init__(self):
+        super (AntiFlash, self).__init__ ()
+        print("Antiflash enabled...")
+
+    def zero_flash(self):
+        while not QUIT:
+            flashed = pm.read_int(player + m_flFlashMaxAlpha)
+            if flashed > 0:
+                pm.write_int(player + m_flFlashMaxAlpha, 0)
+
+class Wallhack(threading.Thread):
+    def __init__(self):
+        super (Wallhack, self).__init__ ()
+        self.r = 255
+        self.g = 0
+        self.b = 0
+        print("Wallhack enabled...")
 
     def _wh(self):
-        player_team = int(pm.read_int(player + m_iTeamNum))
-        while self.run_event.is_set():
-            time.sleep(0.0001)
-            for i in range(1, 32):  # entities 1-32 are reserved for players
-                entity = self.pm.read_int(self.client + dwEntityList + i * 0x10)
+        glow_manager = pm.read_int(client + dwGlowObjectManager)
+        while not QUIT:
+            for i in range(1,10): # for competitive otherwise should be range(1, 32)
+                entity = pm.read_int(client + dwEntityList + i * 0x10)
                 if entity:
-                    is_ennemy = self.pm.read_int(entity + m_iTeamNum)
-                    entity_glow = self.pm.read_int(entity + m_iGlowIndex)
-                    if player_team != is_ennemy:  # wh only on ennemy team
-                        self.pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(255))  # R 
-                        self.pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))  # G
-                        self.pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))  # B
-                        self.pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(0.5))  # Alpha
-                        self.pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)
+                    player_id = int(pm.read_int(player + m_iTeamNum))
+                    entity_id = pm.read_int(entity + m_iTeamNum)
+                    entity_glow = pm.read_int(entity + m_iGlowIndex)
+                    if is_ennemy(player_id, entity_id):  # wh only on ennemy team
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(self.r))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(self.g))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(self.b))
+                        pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(0.5))  # Alpha
+                        pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)
 
+class Trigger(threading.Thread):
+    def __init__(self):
+        super (Trigger, self).__init__ ()
 
-class Trigger:
-    def __init__(self, pm=None, client=None):
-        self.pm = pm
-        self.client = client
-        self.run_event = threading.Event()
-        self.run_event.set()
         thread = Thread(target=self._trigger, args=())
         thread.start()
+        print("Trigger enabled... press ALT to use it")
 
     def _trigger(self):
-        player_team = int(pm.read_int(player + m_iTeamNum))
-        while self.run_event.is_set():
-            cr_entity = self.pm.read_int(player + m_iCrosshairId)
-            if cr_entity > 0 and cr_entity <= 64:
-                entity = self.pm.read_int(self.client + dwEntityList + (cr_entity -1) * 0x10)
-                entity_team = self.pm.read_int(entity + m_iTeamNum)
-                if player_team != entity_team:
-                    self.pm.write_int(self.client + dwForceAttack, 5)
-                    self.pm.write_int(self.client + dwForceAttack, 4)
+        while not QUIT:
+            if keyboard.is_pressed("ALT"):
+                player_id = pm.read_int(player + m_iTeamNum)
+                player_entity_id = pm.read_int(player + m_iCrosshairId)
+                if player_entity_id > 0: # if a player is on the crosshair
+                    entity = pm.read_int(client + dwEntityList + (player_entity_id - 1) * 0x10)
+                    entity_id = pm.read_int(entity + m_iTeamNum)
+                    if is_ennemy(player_id, entity_id):
+                        pm.write_int(client + dwForceAttack, 5)
+                        pm.write_int(client + dwForceAttack, 4)
+
+def is_ennemy(_player_id, _ennemy_id):
+    return _player_id != _ennemy_id
 
 
 if __name__ == "__main__":
-    print("Loading...")
     pm = pymem.Pymem("csgo.exe")
     client = pymem.process.module_from_name(pm.process_handle, "client_panorama.dll").lpBaseOfDll
+    os.system("cls")
+
     player = pm.read_int(client + dwLocalPlayer)
-    glow_manager = pm.read_int(client + dwGlowObjectManager)
-    try:
-        trig = Trigger(pm, client)
-        wh = Wallhack(pm, client)
-        print("Running!")
-    except KeyboardInterrupt:
-        exit()
+    if is_updated(offset_update, offsets):
+        print("offsets are up to date!")
+    else:
+        _update(offset_update)
+    input("Press any key to start the script...")
+    trig = Trigger()
+    wh = Wallhack()
+    fl = AntiFlash()
+    trig.start()
+    wh.start()
+    fl.start()
+    print("Running!")
+    input("Press any key to interrupt the program...")
+    QUIT = True
+    trig.join()
+    wh.join()
+    fl.join()
